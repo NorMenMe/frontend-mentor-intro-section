bbc9fb369cd6d88d3353542c7df82b5a06911fcb
nmetz <info@normanmetz.com>
2025-02-23 09:47:19 +0100

    feat(slider) : update the class is-slide-value  with the current index

--- src/scripts/slider.js
+++ src/scripts/slider.js
@@ -2,22 +2,26 @@ class Slider {
   constructor(args) {
     this.buttons = document.querySelectorAll('.slider__button');  
     this.items = [...document.querySelectorAll('.slider__item')]; 
-    this.slider = document.querySelector('.slider__slider'); 
+    this.slider = document.querySelector('.slider__list'); 
     this.currentIndex = 0; 
   }
 
-  // updateSlideValue
-    // pass the nex index to the string slot of slideValue
+  updateSlideValue() {
+    const previousSlideValue = this.slider.classList[1].slice(0,15);
+    const currentSlideValue = previousSlideValue + this.currentIndex;
 
-  // updateShowItem
-    // updateSlideValue
+    this.slider.classList.remove(previousSlideValue);
+    this.slider.classList.add(currentSlideValue);
+  }
 
   // Move to a specific index in the slider
-  moveTo(index) {
-    this.currentIndex = index;
-    if (this.currentIndex < 0) this.currentIndex = this.items.length - 1; // Wrap around to the last item
+  moveTo(updatedIndex) {
+    this.currentIndex = updatedIndex;
     if (this.currentIndex >= this.items.length) this.currentIndex = 0; // Wrap around to the first item
-    // updateShowItem
+    if (this.currentIndex < 0) this.currentIndex = this.items.length - 1; // Wrap around to the last item
+
+    this.showCurrentImage()
+    this.updateSlideValue();
   }
 
   // Navigate to the next slider item
@@ -30,46 +34,15 @@ class Slider {
     this.moveTo(this.currentIndex - 1) 
   };
 
-  // Close the lightbox
-  closeLightbox() {
-    if (this.lightbox) {
-      this.lightbox.remove();
-      this.lightbox = null;
-    }
-  }
-
-  // Initialize the lightbox and bind lightbox controls
-  initLightbox() {
-    if (!document.querySelector('.lightbox')) {
-      document.body.insertAdjacentHTML('beforeend', this.template);
-    }
-    this.lightbox = document.querySelector('.lightbox'); // Lightbox element
-    this.lightboxImage = this.lightbox.querySelector('img'); // Image inside the lightbox
-    this.buttonForward = this.lightbox.querySelector('.lightbox__controls-arrow--next'); // Forward button
-    this.buttonBackward = this.lightbox.querySelector('.lightbox__controls-arrow--prev'); // Backward button
-    this.lightboxButtonClose = this.lightbox.querySelector('.lightbox__button--close'); // Close button
-
-    // Bind events for lightbox navigation and close
-    this.buttonForward.addEventListener('click', this.next);
-    this.buttonBackward.addEventListener('click', this.previous);
-    this.lightboxButtonClose.addEventListener('click', () => this.closeLightbox());
-  }
-
-  // Open the lightbox and set it to the current index
-  openLightbox(index) {
-    this.currentIndex = index;
-    this.initLightbox();
-    this.updateLightboxContent();
-  }
 
   // Bind slider events for navigation and lightbox opening
   bindEvents() {
     this.buttons.forEach((button) => {
       button.addEventListener('click', (event) => {
-        if (button.classList.contains('slider__button--forward')) {
-          this.next(); // Handle forward navigation
-        } else if (button.classList.contains('slider__button--backward')) {
-          this.previous(); // Handle backward navigation
+        if (event.currentTarget.classList.contains('slider__button--forward')) {
+          this.next(); 
+        } else if (event.currentTarget.classList.contains('slider__button--backward')) {
+          this.previous(); 
         }
       });
     });
@@ -81,16 +54,16 @@ class Slider {
   }
 
   // Highlight the current slider item
-  showCurrentImage(index) {
+  showCurrentImage() {
     this.items.forEach((item, itemIndex) => {
-      item.classList.toggle('is-shown', itemIndex === index);
+      item.classList.toggle('is-shown', itemIndex === this.currentIndex);
     });
   }
 
   // Initialize the slider
   init() {
     if (this.items.length > 0) {
-      this.showCurrentImage(this.currentIndex); 
+      this.showCurrentImage(); 
       this.bindEvents(); 
     }
   }
